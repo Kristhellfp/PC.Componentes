6851f671729f970e7d16d92f7f15cf096dc05240
Iskrisss <klfpaz@scl.edu.gt>
2024-05-07 09:15:17 -0600

    Actualizar

---
+++ index/menu_lateral/menu_lateral.js
@@ -0,0 +1,2 @@
+let div_lateral = document.querySelector (".menu_lateral");
+div_lateral.innerHTML='<ul class ="nav"> <li>CPU</li> <li>GPU</li> <li>Disco HDD</li> <li> Unidad SSD</li> <li>RAM</li> <li>Tarjeta Madre</li> </ul>';
