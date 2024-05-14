44d6a51ed92f9347a140a7652180676e2f4acce1
danialuna <dmluna@scl.edu.gt>
2024-05-14 08:33:12 -0600

    actualizacion dania

---
+++ index/header/header.js
@@ -0,0 +1,16 @@
+let header = document.querySelector (".header")
+
+ header.innerHTML = `
+ <h1>Componentes PC</h1>
+ <div class="botones">
+     <button>Inicio</button>
+     <button>info</button>
+     <button>Contacto</button>
+ </div>
+ <div class="busqueda">
+     <input type="text" placeholder="Buscar...">
+     <span class="material-symbols-outlined">
+search
+</span>
+ </div>
+`;
