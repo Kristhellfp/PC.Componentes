4e7a77cabafeb8622b62fb980e2d89724f2efc10
danialuna <dmluna@scl.edu.gt>
2024-05-16 19:32:10 -0600

    version dania 03

---
+++ footer/footer.js
@@ -0,0 +1,42 @@
+let footer = document.querySelector (".footer")
+
+footer.innerHTML = ` 
+
+<div class="footer">
+        <div class="logo">
+            <img src="https://static.wixstatic.com/media/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png/v1/fit/w_2500,h_1330,al_c/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png" alt="Logo">
+        </div>
+        <div class="info">
+            <h3>Desarrolladores: "dmluna" y "klfpaz"</h2>
+            <h3>Grado: 4to Bach en Compu</h2>
+        </div>
+        <div class="social">
+            <img src="https://cdn3.iconfinder.com/data/icons/picons-social/57/06-facebook-512.png" alt="Facebook">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZigtgnsnOhlwEB-CE8_Ww0Kmcc54MAITEktq_-NoJMA&s" alt="Twitter">
+            <img src="https://cdn4.iconfinder.com/data/icons/picons-social/57/38-instagram-2-512.png" alt="Instagram">
+        </div>
+        <div class="buttons">
+            <a href="https://github.com/dalunaaaa">GitHub dmluna</a>
+            <a href="https://github.com/Kristhellfp">GitHub klfpaz</a>
+        </div>
+
+        <div class="links-container">
+        <div class="link-item">
+            <a href="https://www.nvidia.com/es-la/drivers/what-is-gpu-computing/#:~:text=Una%20CPU%20tiene%20unos%20cuantos,varias%20tareas%20al%20mismo%20tiempo">GPU y CPU</a>
+        </div>
+        <div class="link-item">
+            <a href="https://www.xataka.com/basics/memoria-ram-que-sirve-como-mirar-cuanta-tiene-tu-ordenador-movil">Memoria RAM</a>
+        </div>
+        <div class="link-item">
+            <a href="https://definicion.de/tarjeta-madre/">Tarjeta Madre</a>
+        </div>
+        <div class="link-item">
+            <a href="https://www.hp.com/mx-es/shop/tech-takes/que-es-una-unidad-de-estado-solido-ssd#:~:text=Conocida%20como%20unidad%20de%20estado,que%20funciona%20sin%20partes%20m%C3%B3viles">Unidad SSD</a>
+        </div>
+        <div class="link-item">
+            <a href="https://www.crucial.mx/articles/pc-builders/what-is-a-hard-drive#:~:text=Una%20unidad%20de%20disco%20duro%20(HDD)%20est%C3%A1%20compuesta%20de%20una,o%20escribir%20la%20informaci%C3%B3n%20solicitada">Disco HDD</a>
+        </div>
+        </div>
+
+    </div>
+`;
